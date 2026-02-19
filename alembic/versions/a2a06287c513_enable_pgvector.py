4d3b94ba9bcd71784ebef5951266dc9ce99b00b1
Davide P <davide.padeletti@wave-venture.com>
2026-02-19 12:05:18 +0100

    Skip pgvector migration if extension not available on RDS

--- alembic/versions/a2a06287c513_enable_pgvector.py
+++ alembic/versions/a2a06287c513_enable_pgvector.py
@@ -6,11 +6,8 @@ Create Date: 2026-02-16 14:40:35.091071
 
 """
 from typing import Sequence, Union
-
 from alembic import op
 
-
-# revision identifiers, used by Alembic.
 revision: str = 'a2a06287c513'
 down_revision: Union[str, None] = '3e9e19912d63'
 branch_labels: Union[str, Sequence[str], None] = None
@@ -18,7 +15,14 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.execute("CREATE EXTENSION IF NOT EXISTS vector")
+    try:
+        op.execute("CREATE EXTENSION IF NOT EXISTS vector")
+    except Exception:
+        pass  # pgvector non disponibile su questo RDS, skip
+
 
 def downgrade() -> None:
-    op.execute("DROP EXTENSION IF EXISTS vector")
+    try:
+        op.execute("DROP EXTENSION IF EXISTS vector")
+    except Exception:
+        pass
